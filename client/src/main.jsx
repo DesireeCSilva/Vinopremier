69189fd01df4953a964dd675f821d4560cc44bbd
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-30 20:12:09 +0200

    html-from-detail-page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,6 @@ import { router } from './router/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Detail/Detail.css'
+import Calendar from '../../components/Calendar/Calendar.jsx'
 
 const Detail = () => {
   return (
@@ -19,22 +20,22 @@ const Detail = () => {
           </div>
 
           <div className='page-detail__left__supplement'>
-            <img className='page-detail__left__check' src="src/assets/images/check-icon.png" alt="" />
+            <img className='page-detail__left__check' src="src/assets/images/icons/check-icon.png" alt="" />
             <p className='page-detail__left__suptext'>Añadir suplemento de ibéricos</p>
           </div>
 
           <div className='page-detail__left__cart'>
-            <img className='page-detail__left__counter' src="src/assets/images/counter.png" alt="" />
+            <img className='page-detail__left__counter' src="src/assets/images/icons/counter.png" alt="" />
             <p className='page-detail__left__add'>AÑADIR</p>
-            <img src="src/assets/images/cart.png" alt="" />
+            <img src="src/assets/images/icons/cart.png" alt="" />
           </div>
 
           <div className='page-detail__left__extra'>
-              <img className='page-detail__left__extraicon' src="src/assets/images/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Se puede asistir con niños: no</p>
-              <img className='page-detail__left__extraicon' src="src/assets/images/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Disponible versión vegana: sí</p>
-              <img className='page-detail__left__extraicon' src="src/assets/images/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Disponible en inglés: si, con consulta previa</p>
-              <img className='page-detail__left__extraicon' src="src/assets/images/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Se puede asistir con mascotas: no</p>
-              <img className='page-detail__left__extraicon' src="src/assets/images/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Pueden asistir más personas a la cata de las que compraron las entradas: no</p>
+              <img className='page-detail__left__extraicon' src="src/assets/images/icons/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Se puede asistir con niños: no</p>
+              <img className='page-detail__left__extraicon' src="src/assets/images/icons/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Disponible versión vegana: sí</p>
+              <img className='page-detail__left__extraicon' src="src/assets/images/icons/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Disponible en inglés: si, con consulta previa</p>
+              <img className='page-detail__left__extraicon' src="src/assets/images/icons/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Se puede asistir con mascotas: no</p>
+              <img className='page-detail__left__extraicon' src="src/assets/images/icons/o-icon.png" alt="" /><p className='page-detail__left__extratext' >Pueden asistir más personas a la cata de las que compraron las entradas: no</p>
          </div>
 
           <div className='page-detail__left__calendar' >
@@ -44,10 +45,70 @@ const Detail = () => {
 
         </div>
 
-        <div className='page-detail__right'>
+        <section className='page-detail__section01__right'>
+          <div className='page-detail__right__icons'>
+            <div className='page-detail__right__iconscolumn'>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/person-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>1 hora 30min</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/cupsbottle-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>2 Vinos Blancos, 1 vino rosados, 3 vinos tintos</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/barrel-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>Suplemento cata privada: 60€</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/vino.png" alt="" />
+                <p className='page-detail__right__iconstext'>Suplemento ibérico: Jamón, Salchichón, Lomo, Cecina, Chorizo</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/parking.png" alt="" />
+                <p className='page-detail__right__iconstext'>Garaje Silvela C/ Francisco Silvela 21, 28028 Madrid</p>
+              </div>
+            </div>
+
+            <div className='page-detail__right__iconscolumn'>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/boys-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>Aforo (max): 20 personas</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/hands-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>Accesibilidad: no</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/room-icon.png" alt="" />
+                <p className='page-detail__right__iconstext'>Es posible cenar en el establecimiento: sí</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/traductor.png" alt="" />
+                <p className='page-detail__right__iconstext'>Idioma: inglés y castellano</p>
+              </div>
+              <div className='page-detail__right__iconscontainer'>
+                <img src="src/assets/images/diana.png" alt="" />
+                <p className='page-detail__right__iconstext'>C/Francisco Silvela 25, Madrid</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='page-detail__right__text'>
+            <p>En Vinopremier.com, hemos decidido viajar alrededor del mundo y queremos que nos acompañéis en este viaje. 
+Os proponemos compartir con nosotros una agitante cata de vinos internacionales en nuestra tienda Calle Francisco Silvela 25 (Madrid). 
+De la mano de nuestros expertos sumilleres viajaréis por todos los continentes del mundo y conoceréis los mejores vinos de cada país. Un viaje que se inicia en nuestras tiendas y termina en un país lejano a Madrid degustando lo mejor de cada vino. Si queréis dar una vuelta por el mundo no dudéis en venir a vernos y disfrutar del mejor viaje de vuestra vida.
+Se trata de una cata no profesional, para pasar un rato agradable y divertido, no es necesario tener ningún tipo de conocimiento previo.
+La cata de vinos internacionales con degustación de ibéricos en Madrid es una experiencia enológica y gastronómica que combina la diversidad de vinos de diferentes regiones del mundo con la exquisitez de los ibéricos españoles. Durante esta cata, los participantes tendrán la oportunidad de explorar una selección de vinos internacionales de renombre y disfrutar de una variedad de ibéricos de alta calidad.
+La cata de vinos internacionales ofrece la posibilidad de descubrir vinos procedentes de distintas partes del mundo, como Francia, Italia, Estados Unidos, Australia, entre otros. Se presentarán diferentes estilos de vinos, desde blancos frescos y afrutados hasta tintos estructurados y complejos. Cada vino ofrecerá sus propios aromas, sabores y características únicas, proporcionando una experiencia de cata enriquecedora.
+La degustación de ibéricos españoles complementa a la perfección la cata de vinos internacionales. Los ibéricos, como el jamón ibérico de bellota y los embutidos, son productos emblemáticos de la gastronomía española, reconocidos por su sabor y calidad excepcionales. Durante la degustación, se podrán saborear diferentes cortes de jamón ibérico, embutidos y otras delicias ibéricas, realzando los sabores y creando armonías gastronómicas únicas con los vinos.
+Un experto en vinos y gastronomía guiará la cata, brindando información sobre los vinos internacionales seleccionados, sus regiones de origen y los procesos de elaboración. También se proporcionarán consejos sobre cómo apreciar los vinos y cómo maridarlos adecuadamente con los ibéricos, destacando las sinergias entre los sabores y las texturas.
+Esta experiencia de cata de vinos internacionales con degustación de ibéricos en Madrid es ideal para los amantes del vino y la gastronomía que desean descubrir nuevas etiquetas y explorar combinaciones de sabores emocionantes. Permite sumergirse en el mundo del vino internacional mientras se disfruta de la elegancia y el sabor único de los ibéricos españoles.
+La cata de vinos internacionales con degustación de ibéricos en Madrid es una experiencia sensorial que combina la riqueza de los vinos internacionales con la exquisitez de los ibéricos españoles. Guiados por un experto, los participantes podrán explorar una variedad de vinos internacionales y disfrutar de la combinación perfecta con los ibéricos, creando momentos gastronómicos inolvidables. Es una actividad ideal para aquellos que buscan una experiencia culinaria completa y un viaje por los sabores del mundo en el corazón de Madrid.</p>
+          </div>
           
 
-        </div>
+        </section>
 
       </section>
     </>
