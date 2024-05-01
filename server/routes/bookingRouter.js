8ffa1641ac215255a80b09a03985b9c728c1b537
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-01 12:05:11 +0200

    id_user-autocomplete

--- server/routes/bookingRouter.js
+++ server/routes/bookingRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { getAllBookings, createBooking, deleteBooking, updateBooking, getBookingById } from '../controllers/bookingController.js';
-
+import { authToken } from '../middlewares/authMiddleware.js';
 const bookingRouter = express.Router();
 
-bookingRouter.get("/", getAllBookings);
-bookingRouter.post("/", createBooking);
-bookingRouter.delete("/:id", deleteBooking);
-bookingRouter.put("/:id", updateBooking);
-bookingRouter.get("/:id", getBookingById)
+bookingRouter.get("/", authToken, getAllBookings);
+bookingRouter.post("/", authToken, createBooking);
+bookingRouter.delete("/:id", authToken, deleteBooking);
+bookingRouter.put("/:id", authToken, updateBooking);
+bookingRouter.get("/:id", authToken, getBookingById)
 
 export default bookingRouter;
