a19cfa87f53c0311da2dd6dae41da7cf53c4805d
fatima <puntod81@gmail.com>
2024-05-03 12:01:36 +0200

    update-cors-card

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,10 +1,1 @@
 import React from 'react'
-import Calendar from 'react-calendar'
-
-const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
-
-export default Calendar
--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -46,7 +46,6 @@ function Card() {
 
     };
        
-    
         fetchData();
       }, []);
       
