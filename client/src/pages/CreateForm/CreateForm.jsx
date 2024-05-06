1132f7a9a581558f660b65a747c70fb213132026
Andreamartinn17 <andreamartinn17@gmail.com>
2024-05-06 14:29:53 +0200

    create-form-incompleto-y-lo-detesto

--- client/src/pages/CreateForm/CreateForm.jsx
+++ client/src/pages/CreateForm/CreateForm.jsx
@@ -1,64 +1,64 @@
 import React from 'react'
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-hook-form';
-// import { useUserContext } from ''
-// import service
+//import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { postEvent } from '../../services/eventServices'
+
 
 
 const CreateForm = () => {
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    // const {id_user} = useUserContext();
     const navigate = useNavigate();
+    
     
     const onSubmit = async (data) =>{
       try{
-        // const token = localStorage.get('token');
-        //     const id_user = localStorage.get('id_user');
-        //     const response = await post({...data, user:id_user}, token);
-        //     console.log(response)
-        //     alert("Evento creado con éxito");
-        //     navigate("/detail/:${id}");
-        }
-        catch(error){
-          console.error('Error al crear el evento', error);
-  }
-    }
+        console.log("hola")
+        const response = await postEvent(data)
+        return response.data
+        alert("Evento creado correctamente")
+        navigate("/")
+      }
+      catch(error){
+        console.error("Error al crear el evento")
+      }
+    };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Formulario de creación de catas</h2>
       <div className="formCity">
-        <label htmlFor="id_location">Ciudad</label>
-        <input type="text" id="id_location" name="id_location" {...register('id_location', {pattern: /^[a-zA-Z0-9@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, required: true, maxLength:  40})}/>
+        <label htmlFor="id_location">Id de la ciudad</label>
+        <input type="text" id="id_location" name="id_location" {...register('id_location', {required: true})}/>
       </div>
       <div className="formAddress">
         <label htmlFor="address">Dirección</label>
-        {/* <input type="text" id="address" name="address" {...register('address', {pattern: /^[a-zA-Z0-9@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, required: true, maxLength:  40})}/> */}
+        <input type="text" id="address" name="address" {...register('address', {required: true})}/>
       </div>
       <div className="formName">
         <label htmlFor="name">Nombre de la cata</label>
-        <input type="text" id="name" name="name" {...register('name', {pattern: /^[a-zA-Z0-9@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, required: true, maxLength:  40})}/>
+        <input type="text" id="name" name="name" {...register('name', {required: true})}/>
       </div>
       <div className="formDescription">
         <label htmlFor="description">Descripción</label>
-        <input type="text" id="description" name="description" {...register('description', {pattern: /^[a-zA-Z0-9@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, required: true, maxLength:  1000})}/>
+        <input type="text" id="description" name="description" {...register('description', {required: true})}/>
       </div>
       <div className="formType">
         <label htmlFor="cata_type">Tipo de cata</label>
-        <input type="text" id="cata_type" name="cata_type" {...register('cata_type', {pattern: /^[a-zA-Z0-9@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, required: true, maxLength:  40})}/>
+        <input type="text" id="cata_type" name="cata_type" {...register('cata_type', {required: true})}/>
       </div>
       <div className="formPrice">
         <label htmlFor="price">Precio por persona</label>
-        <input type="text" id="price" name="price" {...register('price', {pattern: /^(?![.0]*$)\d+(?:\.\d{1,2})?$/, required: true, maxLength:  40})}/>
+        <input type="text" id="price" name="price" {...register('price', {required: true})}/>
       </div>
       <div className="formPrivate">
         <label htmlFor="private_tasting_supplement">Suplemento de cata privada</label>
-        <input type="text" id="private_tasting_supplement" name="private_tasting_supplement" {...register('private_tasting_supplement', {pattern: /^(?![.0]*$)\d+(?:\.\d{1,2})?$/,required: true,maxLength: 40})}/>
+        <input type="text" id="private_tasting_supplement" name="private_tasting_supplement" {...register('private_tasting_supplement', {required: true})}/>
       </div>
       <div className="formIberian">
         <label htmlFor="iberian_supplement">Suplemento de ibéricos</label>
-        <input type="text" id="iberian_supplement" name="iberian_supplement" {...register('iberian_supplement', {pattern: /^(?![.0]*$)\d+(?:\.\d{1,2})?$/, required: true, maxLength:  40})}/>
+        <input type="text" id="iberian_supplement" name="iberian_supplement" {...register('iberian_supplement', {required: true})}/>
       </div>
       <div className="formTime">
         <label htmlFor="time">Hora</label>
@@ -70,7 +70,7 @@ const CreateForm = () => {
       </div>
       <div className="formCapacity">
         <label htmlFor="capacity">Aforo máximo</label>
-        <input type="text" id="capacity" name="capacity" {...register('', {pattern: /^[0-9]+(?:\.\d{1,2})?$|^\u0024|\u20AC$/, required: true, maxLength:  40})}/>
+        <input type="text" id="capacity" name="capacity" {...register('capacity', {required: true})}/>
       </div>
       <div className="formExtraPeople">
         <label>Pueden asistir más personas a la cata de las que compraron las entradas</label>
@@ -102,7 +102,7 @@ const CreateForm = () => {
       </div>
       <div className="formParking">
         <label htmlFor="parking">Parking</label>
-        <input type="text" id="parking" name="parking" {...register('parking', {pattern: /^[0-9]+(?:\.\d{1,2})?$|^\u0024|\u20AC$/, required: true, maxLength:  40})}/>
+        <input type="text" id="parking" name="parking" {...register('parking', { required: true})}/>
       </div>
       <div className="formAccesibility">
         <label>Accesibilidad</label>
@@ -119,17 +119,17 @@ const CreateForm = () => {
         </select>
       </div>
       <div className="formEnglish">
-        <label>Disponibilidad en inglés</label>
-        <input type="text" {...register('', {pattern: /^[0-9]+(?:\.\d{1,2})?$|^\u0024|\u20AC$/, required: true, maxLength:  40})}/>
+        <label htmlFor="english">Disponibilidad en inglés</label>
+        <input type="text" id="english" name="english" {...register('english', { required: true})}/>
       </div>
       <div className="formImage">
-        <label htmlFor="imageURL">Añadir imagen de la cata</label>
-        <input type="url" id="imageURL" name="ImageURL" {...register('imageURL', { required: true})}/>
+        <label htmlFor="image">Añadir imagen de la cata</label>
+        <input type="url" id="image" name="image" {...register('image', { required: true})}/>
       </div>
-      <input className="buttonCreate" type="submit" value="ENVIAR"/>
+      <button className="buttonCreate" type="submit">ENVIAR</button>
     </form> 
     </div>
   )
 }
 
-export default CreateForm
+export default CreateForm;
