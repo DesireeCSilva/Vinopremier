aab1d80d8bd9925fad9c08e73761189b12a44ba7
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-06 14:13:41 +0200

    create-validate-helper

---
+++ server/helpers/validateHelper.js
@@ -0,0 +1,9 @@
+import { validationResult } from "express-validator";
+
+export const handleValidationResults = (request, response, next) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(422).json({ errors: errors.array() });
+    }
+    next();
+};
