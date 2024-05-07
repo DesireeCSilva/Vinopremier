566b8c5f28a8d7861b7f00c007f86e580e73b948
DesireeCSilva <desiresilva1987@gmail.com>
2024-05-07 22:10:15 +0200

    CSS-Register-form-done

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -25,24 +25,24 @@ const RegisterForm = () => {
   return (
     <>
     <form onSubmit={handleSubmit}>
-      <div className='register-form_title'>
+      <div className='register-form_titlebox'>
         <h2 className='register-form_title'>CREAR NUEVA CUENTA DE CLIENTE</h2>
       </div>
 
       <section className='register-form' >
         <div className='register-form_column'>
-      <div className='register-form_label'>
-        <label htmlFor="name">
+      <div>
+        <label className='register-form_label' htmlFor="name">
           Nombre y Apellidos
-          <br /><input type="text" value={name} onChange={(e) => {
+          <br /><input className='register-form_input' type="text" value={name} onChange={(e) => {
             setName(e.target.value)}} required></input>
         </label>
       </div>
 
-      <div className='register-form_label'>
-        <label htmlFor='email'>
+      <div>
+        <label className='register-form_label'  htmlFor='email'>
           Email
-          <br /><input type="email" value={email} onChange={(e) =>{
+          <br /><input className='register-form_input' type="email" value={email} onChange={(e) =>{
             setEmail(e.target.value);
           }} required></input>
         </label>
@@ -50,19 +50,19 @@ const RegisterForm = () => {
         </div>
 
         <div className='register-form_column'>
-      <div className='register-form_label'>
-        <label htmlFor='phone'>
+        <div>
+        <label className='register-form_label'  htmlFor='phone'>
           Teléfono
-          <br /><input type="text" value={phone} onChange={(e) => {
+          <br /><input className='register-form_input' type="text" value={phone} onChange={(e) => {
             setPhone(e.target.value);
           }} required></input>
         </label>
       </div>
 
-      <div className='register-form_label'>
-        <label htmlFor='password'>
+      <div>
+        <label className='register-form_label'  htmlFor='password'>
           Contraseña
-          <br /><input type="password" value={password} onChange={(e) => {
+          <br /><input className='register-form_input' type="password" value={password} onChange={(e) => {
             setPassword(e.target.password);
           }} required></input>
         </label>
