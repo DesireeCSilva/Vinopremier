10e3affa57b2e8896562e3a3fa12ede4b83afbdd
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-07 15:37:13 +0200

    create-register-form

--- client/src/pages/RegisterForm/RegisterForm.jsx
+++ client/src/pages/RegisterForm/RegisterForm.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { createUser } from '../../services/authServices';
+import { Link, useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const data = await createUser(name, email, phone, password);
+      alert('Usuario registrado correctamente');
+      localStorage.setItem('token', data.token);
+      console.log(localStorage.getItem('token'));
+
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
   return (
-    <div>RegisterForm</div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">
+          Nombre y Apellidos
+          <input type="text" value={name} onChange={(e) => {
+            setName(e.target.value)}} required></input>
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor='email'>
+          Email
+          <input type="email" value={email} onChange={(e) =>{
+            setEmail(e.target.value);
+          }} required></input>
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor='phone'>
+          Teléfono
+          <input type="text" value={phone} onChange={(e) => {
+            setPhone(e.target.value);
+          }} required></input>
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor='password'>
+          Contraseña
+          <input type="password" value={password} onChange={(e) => {
+            setPassword(e.target.password);
+          }} required></input>
+        </label>
+      </div>
+      <button type="submit">Regístrate</button>
+    </form>
+    </>
   )
 }
 
