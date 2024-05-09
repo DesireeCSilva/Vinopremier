9f823314fbb4fd45e23b858c11f39ae9fe55f86a
DesireeCSilva <desiresilva1987@gmail.com>
2024-05-09 17:37:34 +0200

    CSS-payment-page-doing

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../Payment/Payment.css'
 
 const Payment = () => {
   return (
-    <div>Payment</div>
+    <section className='payment-page'>
+      <img className='payment-page_img' src="/src/assets/images/banners/payment-left.PNG" alt="" />
+      
+      <div className='payment-page_right-container'>
+        <p className='payment-page_right-title'>RESUMEN DE COMPRA</p>
+        <hr className='payment-page_right-hr' />
+        <p>Subtot</p>
+
+      </div>
+
+    </section>
   )
 }
 
