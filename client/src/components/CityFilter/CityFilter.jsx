dd8cbf0c5cc87178d8b1a24fe2a8f4907ce35b6b
fatima <puntod81@gmail.com>
2024-05-09 14:04:53 +0200

    update-filter

--- client/src/components/CityFilter/CityFilter.jsx
+++ client/src/components/CityFilter/CityFilter.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CityFilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+`
 
 const CityFilter = () => {
   return (
-    <div>CityFilter</div>
+    <>
+    <CityFilterContainer>
+    <div className='name-filter'>
+      <h1 className='tittle-filter'>Catas y Eventos en Madrid</h1>
+      <img src='../../src/assets/images/cities/madrid.png' alt='Madrid' />
+
+    </div>
+
+    <div className='name-filter'>
+      <h1 className='tittle-filter'>Catas y Eventos en Mallorca</h1>
+      <img src='../../src/assets/images/cities/mallorca.png' alt='Mallorca' />
+
+    </div>
+
+    <div className='name-filter'>
+    <h1 className='tittle-filter'>Catas y Eventos en Zaragoza</h1>
+    <img src='../../src/assets/images/cities/zaragoza.png' alt='Zaragoza' />
+    </div> 
+    </CityFilterContainer>
+    </>
   )
 }
 
