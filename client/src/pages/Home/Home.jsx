5204ee3d41ccb70becb930cdefc0859d214ea489
fatima <puntod81@gmail.com>
2024-05-10 09:43:21 +0200

    city-filter

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Card from '../../components/card/card';
+import CityFilterContainer from '../../components/CityFilter/CityFilter';
 import  '../../components/Calendar/customcalendar.css';
 
 
 const Home = () => {
   return (
     <>
-     
+    <CityFilterContainer/>
     <Card/>
+    
     
     </>
   )
