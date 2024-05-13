0395bd3901ff2cfcab965e96a3c64f7f6b10b5f1
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-13 10:59:20 +0200

    change-edit-form

--- server/routes/eventRouter.js
+++ server/routes/eventRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getAllEvents, createEvent, deleteEvent, updateEvent, getEventById, getEventByName, getEventDatesByName } from "../controllers/eventController.js";
+import { getAllEvents, createEvent, deleteEvent, updateEvent, getEventById, getEventByName, getEventDatesByName, updateEventByName } from "../controllers/eventController.js";
 
 const eventRouter = express.Router();
 
 eventRouter.get("/", getAllEvents);
 eventRouter.post("/", createEvent);
 eventRouter.delete("/:id", deleteEvent);
+eventRouter.put("/:name", updateEventByName)
 eventRouter.put("/:id", updateEvent);
 eventRouter.get("/name", getEventByName);
 eventRouter.get("/:eventName/dates", getEventDatesByName)
