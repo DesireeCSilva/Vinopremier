3cfa5dd54e24ee5f9528d2c5304fdb7b72e1135a
albamartinmz <albamartinsonido@gmail.com>
2024-05-14 14:21:41 +0200

    pass-register-and-login-controller-tests

--- server/tests/test-helper/helperTest.js
+++ server/tests/test-helper/helperTest.js
@@ -1,10 +1,29 @@
-import UserModel from '../../models/UserModel.js';
+export const testUser = {
+    "name": "test user",
+    "email": "test@gmail.com",
+    "phone": "666666666",
+    "password": "test1234"
+}
 
-export const createUserTest = async () => {
-    return await UserModel.create({
-        name: 'Test User',
-        email: 'test@example.com',
-        phone: '666666666',
-        password: '1234',
-    });
-};
+export const testAdmin = {
+    "name": "test admin",
+    "email": "test@gmail.com",
+    "phone": "666666666",
+    "password": "test1234",
+    "role": "admin"
+}
+
+export const loginUser = {
+    "email": "test@gmail.com",
+    "password": "test1234"
+}
+
+export const invalidUser = {
+    "email": "invalidemail@example.com",
+    "password": "invalidpassword"
+}
+
+export const invalidPasswordUser = { 
+    email: loginUser.email, 
+    password: 'incorrectpassword' 
+}
