a7d717958787297fad4128bcbb6fa9664e8cb28c
DesireeCSilva <desiresilva1987@gmail.com>
2024-05-15 17:40:17 +0200

    add-changes-on-README

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../Payment/Payment.css'
 
-// const handleCheckboxChange = (event) => {
 //   const { name, checked } = event.target;
 //   setIsChecked(prevState => ({ ...prevState, [name]: checked }));
 // }
