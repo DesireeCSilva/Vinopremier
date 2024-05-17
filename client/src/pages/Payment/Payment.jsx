45103bd260fd1f31a206b3858ada7aa49f012133
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-17 13:32:06 +0200

    add-delete-Booking-in-payment

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '../Payment/Payment.css';
-import { getAllBookingsByUser } from '../../services/bookingServices.js';
+import { getAllBookingsByUser, deleteBooking } from '../../services/bookingServices.js';
+import { getEventById } from '../../services/eventServices.js';
 import { useParams } from 'react-router-dom';
 
 const Payment = () => {
   const { id_user } = useParams();
-  const [bookingData, setBookingData] = useState(null);
+  const [bookingData, setBookingData] = useState([]);
+  const [eventsData, setEventsData] = useState([]);
 
   useEffect(() => {
 
@@ -13,6 +15,7 @@ const Payment = () => {
       try {
         const response = await getAllBookingsByUser(id_user);
         setBookingData(response);
+        console.log(response);
       } catch(error) {
         console.error("Error al cargar los datos de la reserva", error);
 
@@ -20,12 +23,39 @@ const Payment = () => {
     };
     fetchBookingByUser();
   }, [id_user]);
+
+  useEffect(() => {
+    const fetchEventsData = async() => {
+      try {
+        const eventsPromises = bookingData.map(async (booking) => {
+          const eventResponse = await getEventById(booking.id_event);
+          return eventResponse;
+        });
+        const eventsData = await Promise.all(eventsPromises);
+        setEventsData(eventsData);
+      } catch (error) {
+        console.error("Error al cargar los datos de los eventos", error)
+      }
+    };
+
+    fetchEventsData();
+  }, [bookingData]);
+
+  const handleDeleteBooking = async (bookingId) => {
+    try {
+      await deleteBooking(bookingId);
+      const updatedBookings = bookingData.filter(booking => booking.id !== bookingId);
+      setBookingData(updatedBookings);
+    } catch (error) {
+      console.error("Error al eliminar la reserva", error);
+    }
+  };
+
   return (
     <>
-    <section className='payment-page'>
-      <img className='payment-page_img' src="/src/assets/images/banners/payment-left.PNG" alt="" />
-      
-      <article>
+      <section className='payment-page'>
+        <img className='payment-page_img' src="/src/assets/images/banners/payment-left.PNG" alt="" />
+        <article>
         <p className='payment-page_right-title'>RESUMEN DE COMPRA</p>
         
         <div className='payment-page_right-container'>
@@ -44,21 +74,33 @@ const Payment = () => {
             <p>Total del pedido</p>
             <p>€</p>
           </div>
-          <p className='payment-page_right-text'>ARTÍCULOS EN EL CARRITO</p>
-          <hr className='payment-page_right-hr' />
-          <div className='payment-page_right-article'>
-            <img className='payment-page_right-img02' src="/src/assets/images/cards-original/card01.jpg" alt="" />
-            <p className='payment-page_right-article-text'>Nombre Cata
-            <p>€</p>
-            </p>
-            
+            <p className='payment-page_right-text'>ARTÍCULOS EN EL CARRITO: {bookingData.length}</p>
+            <hr className='payment-page_right-hr' />
+            {bookingData.map((booking, index) => (
+              <div key={booking.id} className='payment-page_right-article'>
+                <img className='payment-page_right-img02' src={eventsData[index]?.image || "/src/assets/images/cards-original/card01.jpg"} alt="Imagen de la cata" />
+                <div className='payment-page_right-article-text'>
+                  <p>{eventsData[index]?.name || booking.id_event}</p>
+                  <p>Precio Final: €{parseFloat(booking.final_price).toFixed(2)}</p>
+                  <p>Número de Personas: {booking.people}</p>
+                  {booking.vegan_people > 0 && (
+                    <p>Personas Veganas: {booking.vegan_people}</p>
+                  )}
+                  {booking.iberian_supplement > 0 && (
+                    <p>Suplemento Ibérico: Sí</p>
+                  )}
+                  {booking.private_tasting_supplement && (
+                    <p>Suplemento de Cata Privada: Sí</p>
+                  )}
+                </div>
+                <button onClick={() => handleDeleteBooking(booking.id)}>Eliminar pedido</button>
+              </div>
+            ))}
           </div>
-        </div>
-      </article>
-    </section>
-
+        </article>
+      </section>
     </>
-  )
+  );
 }
 
 export default Payment
