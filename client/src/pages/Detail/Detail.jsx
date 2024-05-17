d5ed380386c3a6cfc8c3be09ddd9ca6c074a46f8
Andreamartinn17 <andreamartinn17@gmail.com>
2024-05-17 15:28:12 +0200

    nothing

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -281,7 +281,7 @@ const handlePayment = () => {
             )}
             {isAuthenticated && (
               <>
-            <button style={{ cursor: 'pointer', float: 'right', padding:'1.5vw', margin:'2vw', backgroundColor:'#ffffff',color: '#000000',border:'4px solid #000000' , fontWeight:'bold', fontSize:'2vw'}} onClick={handleDateForm}>AÑADIR NUEVA FECHA</button>
+            <button onClick={handleDateForm}>AÑADIR NUEVA FECHA</button>
             </>
             )}
           </div>
