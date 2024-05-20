654cfc3cf12db907890b2cd0eabe6df4d678c6e1
SarayAnta <s.rodriguezanta@gmail.com>
2024-05-20 13:56:06 +0200

    add-middlewares-to-back-routes

--- server/routes/bookingRouter.js
+++ server/routes/bookingRouter.js
@@ -2,13 +2,15 @@ import express from 'express';
 import { getAllBookings, createBooking, deleteBooking, updateBooking, getBookingById, getAllBookingsByUser } from '../controllers/bookingController.js';
 import { authToken } from '../middlewares/authMiddleware.js';
 import authRole from '../middlewares/roleMiddleware.js';
+import { bookingValidator } from '../validators/bookingsValidator.js';
+import { handleValidationResults } from '../helpers/validateHelper.js';
 const bookingRouter = express.Router();
 
 bookingRouter.get("/", authToken, authRole(['admin']), getAllBookings);
-bookingRouter.post("/", authToken, createBooking);
-bookingRouter.delete("/:id", authToken, deleteBooking);
+bookingRouter.post("/", authToken, authRole(['user', 'admin', 'superadmin']), bookingValidator, handleValidationResults, createBooking);
+bookingRouter.delete("/:id", authToken, authRole(['user', 'admin', 'superadmin']), deleteBooking);
 bookingRouter.put("/:id", authToken, updateBooking);
 bookingRouter.get("/:id", authToken, getBookingById)
-bookingRouter.get("/user/:id_user", authToken, getAllBookingsByUser)
+bookingRouter.get("/user/:id_user", authToken, authRole(['user', 'admin', 'superadmin']), getAllBookingsByUser)
 
 export default bookingRouter;
--- server/routes/locationRouter.js
+++ server/routes/locationRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getAllLocations, deleteLocation, updateLocation, createLocation, getLocationById } from '../controllers/locationController.js';
 import { authToken } from '../middlewares/authMiddleware.js';
+import { locationValidator } from '../validators/locationsValidator.js';
+import { handleValidationResults } from '../helpers/validateHelper.js';
 
 const locationRouter = express.Router();
 
 locationRouter.get("/", getAllLocations);
-locationRouter.post("/", authToken, createLocation);
+locationRouter.post("/", authToken, locationValidator, handleValidationResults, createLocation);
 locationRouter.delete("/:id", authToken, deleteLocation);
 locationRouter.put("/:id", authToken, updateLocation);
 locationRouter.get("/:id", getLocationById);
