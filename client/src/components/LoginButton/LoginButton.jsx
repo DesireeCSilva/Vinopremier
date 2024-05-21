3cdb81a378e05e6b2a3eda3ed2c64d780fb07a19
fatima <puntod81@gmail.com>
2024-05-21 13:03:04 +0200

    styled-login

--- client/src/components/LoginButton/LoginButton.jsx
+++ client/src/components/LoginButton/LoginButton.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
+
+
+
 const LoginButton = () => {
     const navigate = useNavigate();
 
     return (
-    <div>
-        <button style={{ cursor:'pointer', float: 'right', fontFamily: 'Gotham', fontSize: '1rem', color:'#fff', background:'#000', border: 'none', padding:'2%', marginTop: '2rem', height: '4.4rem', cursor: 'pointer', letterSpacing: '0.09em', marginRight:'2.1rem', background:'#AC946A'}} onClick={() => navigate (`/login`)} >INICIA SESIÓN</button>
+        <div>
+        <button style={{ 
+            cursor:'pointer', 
+            float: 'right', 
+            fontFamily: 'Gotham', 
+            fontSize: '1.25vw', 
+            color:'#fff', 
+            background:'#000', 
+            border: 'none', 
+            padding:'1vw', 
+            marginTop: '2.5vw', 
+            
+            cursor: 'pointer', 
+            letterSpacing: '0.09em', 
+            marginRight:'2.1rem', 
+            background:'#AC946A',
+            textAlign: 'center' 
+        }} onClick={() => navigate (`/login`)} >INICIA SESIÓN</button>
     </div>
     )
 }
